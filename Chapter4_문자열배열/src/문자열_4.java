2d0a00f33d2311afdb37bbb1c85ef764e22ae3b9
SIST <SIST@DESKTOP-J1EES6D>
2023-04-10 09:01:19 +0900

    20230407

---
+++ Chapter4_문자열배열/src/문자열_4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/*
+ * length() => 문자 갯수 =>
+ * trim() ==> 좌우 공백 제거
+ * javascript => java 동일 => jquery / vuejs / reactjs
+ */
+
+public class 문자열_4 {
+	public static void main(String[] args) {
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.print("문자열 입력:");
+//		String s = scanner.next();
+//		
+//		System.out.println("입력된 값:"+s);
+//		System.out.println("문자 갯수:"+s.length());
+		
+		String s = "Hello Java!! ";
+		System.out.println(s.length());
+		System.out.println(s.trim().length());
+		
+	}
+}
