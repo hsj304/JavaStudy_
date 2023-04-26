7e350f96ec5221c1d87c1753cb3d4f29d63f9e23
SIST <SIST@DESKTOP-J1EES6D>
2023-04-26 10:04:16 +0900

    20230424

---
+++ Chapter9_라이브러리_1/src/com/sist/lib/라이브러리_1.java
@@ -0,0 +1,26 @@
+package com.sist.lib;
+/*
+ * 	1.java.lang
+ * 		Objec : 모든 클래스의 상위 클래스 ( 모든 클래스는 상속을 Object로 받는다)
+ * 			기능(메소드)
+ * 	2.java.util
+ * 	2-1.java.text
+ * 	3.java.io
+ * 	4.java.net
+ * 	5.java.sql
+ * 	------------- 기본 라이브러리
+ * 	6.javax.xml
+ * 	7.javax.servlet.http
+ * 	8.외부 라이브러리(자바에서 지원하는 것이 아니라 업체) - Open API => Jsoup, Simple-json : mvnrepository.com
+ * 		1. DB(Mybatis, Hibernate(JPA))
+ * 		2. XML, JSON(VueJS, Ajax, React)
+ * 		3. 프레임워크 : Spring / Spring-Boot / Struts
+ * 		*** JSP(HTML = ThymeLeaf)
+ * 		현대 => Spring Boot / React
+ * 		삼성 => Spring Boot / VueJS   
+ */
+public class 라이브러리_1 {
+	public static void main(String[] args) {
+		
+	}
+}
